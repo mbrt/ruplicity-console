2d0b1320eb10462a09115ee9e5d3358ae3cd4dce
Michele Bertasi <michele.bertasi@gmail.com>
2015-12-14 13:51:12 +0000

    Added clippy plugin and travis badge

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ruplicity-console
+[![Build Status](https://travis-ci.org/mbrt/ruplicity-console.svg?branch=master)](https://travis-ci.org/mbrt/ruplicity-console)
+
 Command line interface for ruplicity
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![deny(missing_copy_implementations,
+        trivial_casts, trivial_numeric_casts,
+        unsafe_code,
+        unstable_features,
+        unused_import_braces, unused_qualifications)]
+
+#![cfg_attr(feature = "nightly", allow(unstable_features))]
+#![cfg_attr(feature = "lints", feature(plugin))]
+#![cfg_attr(feature = "lints", plugin(clippy))]
+
 extern crate ruplicity;
 #[macro_use]
 extern crate clap;
